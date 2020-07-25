6b36fee2fe396ae7ff7fdb5ad45355533cf3e0d9
Sheryaar <Sheryaar.butt@gmail.com>
2020-07-24 20:18:56 -0400

    -Initial Commit

---
+++ src/main/java/com/sharueigo/bootstrapcourse/BootstrapCourseApplication.java
@@ -0,0 +1,13 @@
+package com.sharueigo.bootstrapcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharueigo/bootstrapcourse/BootstrapCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharueigo.bootstrapcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
